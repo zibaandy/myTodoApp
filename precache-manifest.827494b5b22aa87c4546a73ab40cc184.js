0e9166f7bf5626228ba5876518223cf603d63b70
zibaandy <zibaandy20@gmail.com>
2018-11-04 20:54:53 -0800

    Updates

---
+++ precache-manifest.827494b5b22aa87c4546a73ab40cc184.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d370010f724d9cb65594",
+    "url": "/myTodoApp/static/js/runtime~main.d370010f.js"
+  },
+  {
+    "revision": "2356abf20c04eded438b",
+    "url": "/myTodoApp/static/js/main.2356abf2.chunk.js"
+  },
+  {
+    "revision": "5016b5e12a80c0c9246e",
+    "url": "/myTodoApp/static/js/1.5016b5e1.chunk.js"
+  },
+  {
+    "revision": "2356abf20c04eded438b",
+    "url": "/myTodoApp/static/css/main.7f786d43.chunk.css"
+  }
+];
